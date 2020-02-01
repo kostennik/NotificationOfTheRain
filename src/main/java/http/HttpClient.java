caeb041da5abffa1268316b86029b594923454d8
Kostennik <kostennik@gmail.com>
2020-02-01 15:03:21 +0100

    add http client

--- src/main/java/http/HttpClient.java
+++ src/main/java/http/HttpClient.java
@@ -1,4 +1,74 @@
 package http;
 
+import javax.net.ssl.HttpsURLConnection;
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 public class HttpClient {
+
+    //HTTP GET request
+    public String sendGet(String url) throws Exception {
+        URL obj = new URL(url);
+        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+
+        //optional default is GET
+        con.setRequestMethod("GET");
+        int responseCode = con.getResponseCode();
+        System.out.println("\nSending 'GET' request to URL : " + url);
+        System.out.println("Response Code : " + responseCode);
+
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(con.getInputStream()));
+        String inputLine;
+        StringBuffer response = new StringBuffer();
+
+        while ((inputLine = in.readLine()) != null) {
+            response.append(inputLine);
+        }
+        in.close();
+
+        //print result
+        System.out.println(response.toString());
+        return response.toString();
+
+    }
+
+    //HTTP POST request
+    public String sendPost(String url, String request, String contentType) throws IOException {
+
+        URL obj = new URL(url);
+        HttpsURLConnection con = (HttpsURLConnection) obj.openConnection();
+
+        //add request header
+        con.setRequestMethod("POST");
+        con.setRequestProperty("Accept-Language", "en-US,en;q=0.5");
+        con.setRequestProperty("Content-Type", contentType);
+        con.setRequestProperty("Accept-Charset", "UTF-8");
+        con.setRequestProperty("Content-Length", String.valueOf(request.length()));
+
+        //Send post request
+        con.setDoOutput(true);
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(con.getOutputStream()));
+
+        //write data
+        bw.write(request);
+        bw.flush();
+        bw.close();
+
+        int responseCode = con.getResponseCode();
+        System.out.println("\nSending 'POST' request to URL : " + url);
+        System.out.println("Post parameters : " + request);
+        System.out.println("Response Code  : " + responseCode);
+
+        //save response as json
+        BufferedReader br = new BufferedReader(new InputStreamReader((con.getInputStream())));
+        StringBuilder sb = new StringBuilder();
+        String output;
+        while ((output = br.readLine()) != null) {
+            sb.append(output);
+        }
+        System.out.println(sb);
+        return sb.toString();
+    }
 }
