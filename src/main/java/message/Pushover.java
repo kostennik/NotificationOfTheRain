fb1379e8f1ea0d6d17731ede595786f140edfd02
Kostennik <kostennik@gmail.com>
2020-02-03 17:10:31 +0100

    add Pushover client

--- src/main/java/message/Pushover.java
+++ src/main/java/message/Pushover.java
@@ -1,4 +1,30 @@
 package message;
 
+import http.HttpClient;
+
 public class Pushover {
+    private final String token;
+    private final HttpClient httpClient = new HttpClient();
+
+    public Pushover(String token) {
+        this.token = token;
+
+    }
+
+    public String  sendMessage(String user, String  title, String message) throws Exception {
+
+        StringBuilder request = new StringBuilder();
+        request.append("token=").append(token)
+                .append("&user=").append(user)
+                .append("&device=droid")
+                .append("&title=").append(title)
+                .append("&message=").append(message);
+
+        String url = "https://api.pushover.net/1/messages.json";
+        String contentType = "application/x-www-form-urlencoded";
+
+        //send data
+        String response = httpClient.sendPost(url, request.toString(), contentType);
+        return response;
+    }
 }
