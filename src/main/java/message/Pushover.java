bd30ed2570f5ae7b7eba8528496cd6853d0e49b8
Kostennik <kostennik@gmail.com>
2020-02-04 19:37:04 +0100

    fix

--- src/main/java/message/Pushover.java
+++ src/main/java/message/Pushover.java
@@ -3,6 +3,9 @@ package message;
 import http.HttpClient;
 
 public class Pushover {
+    private final String url = "https://api.pushover.net/1/messages.json";
+    private final String contentType = "application/x-www-form-urlencoded";
+
     private final String token;
     private final HttpClient httpClient = new HttpClient();
 
@@ -11,17 +14,14 @@ public class Pushover {
 
     }
 
-    public String  sendMessage(String user, String  title, String message) throws Exception {
+    public String sendMessage(String user, String title, String message) throws Exception {
 
         StringBuilder request = new StringBuilder();
         request.append("token=").append(token)
                 .append("&user=").append(user)
-                .append("&device=droid")
                 .append("&title=").append(title)
                 .append("&message=").append(message);
 
-        String url = "https://api.pushover.net/1/messages.json";
-        String contentType = "application/x-www-form-urlencoded";
 
         //send data
         String response = httpClient.sendPost(url, request.toString(), contentType);
